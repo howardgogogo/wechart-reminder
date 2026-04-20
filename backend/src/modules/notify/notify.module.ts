c7e2d3a29944221a025d7168a8f3242be3eed213
howard <2225813275@qq.com>
2026-04-20 18:10:02 +0800

    Fix circular dependency, add enums file

--- backend/src/modules/notify/notify.module.ts
+++ backend/src/modules/notify/notify.module.ts
@@ -5,9 +5,10 @@ import { WechatEnterpriseService } from './wechat-enterprise.service';
 import { ReminderModule } from '../reminder/reminder.module';
 import { ReminderCronService } from '../../shared/cron/reminder-cron.service';
 import { ReminderLog } from '../reminder/reminder-log.entity';
+import { LunarModule } from '../../shared/lunar/lunar.module';
 
 @Module({
-  imports: [ReminderModule, TypeOrmModule.forFeature([ReminderLog])],
+  imports: [ReminderModule, TypeOrmModule.forFeature([ReminderLog]), LunarModule],
   providers: [NotifyService, WechatEnterpriseService, ReminderCronService],
   exports: [NotifyService],
 })
